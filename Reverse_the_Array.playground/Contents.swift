81724fec0baf686b98037a7fc685cabfb412ba3c
abhishek <abhishekdhiman1408@gmail.com>
2022-07-14 19:12:49 +0530

    day 1

--- Reverse_the_Array.playground/Contents.swift
+++ Reverse_the_Array.playground/Contents.swift
@@ -1,4 +1,24 @@
-import UIKit
 
-var greeting = "Hello, playground"
 
+//https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+
+
+let givenArray = [8,4,0,1,5]
+
+
+func reverseTheArray(for numArray: [Int]) -> [Int]{
+    var resultArry = [Int]()
+    
+    for (index,_) in numArray.enumerated(){
+        let oldValueIndex = (numArray.count - 1) - index
+        let valueToReverse = numArray[oldValueIndex]
+        resultArry.append(valueToReverse)
+    }
+    print(resultArry)
+    print("",terminator: " ")
+    print(givenArray)
+   
+    return resultArry
+}
+
+reverseTheArray(for: givenArray)
